c5ed5563f14d51cae3bf3ff0db9ec72fc93fed67
Huy2507 <giahuy5ctv2@gmail.com>
2025-02-18 13:38:39 +0700

    Fix Bug to Deploy 2

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,7 +1,8 @@
-import 'prettier-plugin-tailwindcss';
+import "prettier-plugin-tailwindcss";
 
 export default {
-  plugins: ['prettier-plugin-tailwindcss'],
-  tailwindConfig: './tailwind.config.mjs',
-  tabWidth: 4,
+    plugins: ["prettier-plugin-tailwindcss"],
+    tailwindConfig: "./tailwind.config.mjs",
+    tabWidth: 4,
+    trailingComma: "none",
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.jsx";
@@ -5,9 +6,11 @@ import { CartProvider } from "./components/CartContext.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <Router>
-        <CartProvider>
-            <App />
-        </CartProvider>
-    </Router>,
+    <React.StrictMode>
+        <Router>
+            <CartProvider>
+                <App />
+            </CartProvider>
+        </Router>
+    </React.StrictMode>
 );
